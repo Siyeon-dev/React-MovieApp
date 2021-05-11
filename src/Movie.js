56e0cfdc74e0d447a8a252c16ff339ccb037c530
Siyeon-dev <siyeon.dev@gmail.com>
2021-05-12 08:26:39 +0900

    Adding Styles

--- src/Movie.js
+++ src/Movie.js
@@ -8,14 +8,14 @@ function Movie({ year, title, summary, poster, genres }) {
 			<div className='movie__data'>
 				<h3 className='movie__title'>{title}</h3>
 				<h5 className='movie__year'>{year}</h5>
-				<ul className='genres'>
+				<ul className='movie__genres'>
 					{genres.map((genre, index) => (
 						<li key={index} className='genres__genre'>
 							{genre}
 						</li>
 					))}
 				</ul>
-				<p className='movie__summary'>{summary}</p>
+				<p className='movie__summary'>{summary.slice(0, 180)}...</p>
 			</div>
 		</div>
 	);
